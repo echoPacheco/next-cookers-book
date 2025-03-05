f6f93620f9ad975070c200da9f028b696ae98c21
echoPacheco <mateuspacheco2201@gmail.com>
2025-03-04 17:29:41 -0800

    add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -10,21 +10,21 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
 
   return (
     <>
-      <div className="flex flex-col justify-around rounded-2xl shadow-lg shadow-zinc-900 max-w-sm transform hover:scale-105 transition-transform duration-300">
+      <div className="flex max-w-sm transform flex-col justify-around rounded-2xl shadow-lg shadow-zinc-900 transition-transform duration-300 hover:scale-105">
         <Image
           src={
             typeof recipe_pic === "string"
               ? recipe_pic
-              : "https://morar.com.br/wp-content/uploads/2020/08/Receitas_Rapidas_Morar_Construtora.jpeg"
+              : "/images/default_recipe.webp"
           }
           width={350}
           height={350}
           alt={name || "Default recipe image"}
-          className="w-full max-h-52 object-cover rounded-t-2xl"
+          className="max-h-52 w-full rounded-t-2xl object-cover"
         />
-        <div className="flex justify-between items-center p-2 pb-2">
-          <h2 className="text-xl font-semibold ">{name}</h2>
-          <div className="rounded-full p-2 shadow-md bg-zinc-300">
+        <div className="flex items-center justify-between p-2 pb-2">
+          <h2 className="text-xl font-semibold">{name}</h2>
+          <div className="rounded-full bg-zinc-300 p-2 shadow-md">
             <span className="text-gray-600">{category}</span>
           </div>
         </div>
