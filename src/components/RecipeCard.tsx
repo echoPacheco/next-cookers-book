20aa28f0d4c06f78473490bedcae5264c0d314af
echoPacheco <mateuspacheco2201@gmail.com>
2025-03-17 15:02:00 -0700

    refactor recipe card

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -10,7 +10,7 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
 
   return (
     <>
-      <div className="flex max-w-sm transform flex-col justify-around rounded-2xl shadow-lg shadow-zinc-900 transition-transform duration-300 hover:scale-105">
+      <div className="flex max-w-sm transform flex-col justify-around rounded-xl shadow-lg shadow-zinc-900 transition-transform duration-300 hover:scale-105">
         <Image
           src={
             typeof recipe_pic === "string"
@@ -20,13 +20,15 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
           width={350}
           height={350}
           alt={name || "Default recipe image"}
-          className="max-h-52 w-full rounded-t-2xl object-cover"
+          className="h-44 w-full rounded-t-2xl object-cover md:h-36 xl:h-48"
         />
-        <div className="flex items-center justify-between p-2 pb-2">
-          <h2 className="text-xl font-semibold">{name}</h2>
-          <div className="rounded-full bg-zinc-300 p-2 shadow-md">
-            <span className="text-gray-600">{category}</span>
-          </div>
+        <div className="flex items-center justify-center rounded-xl bg-white p-2 pb-2">
+          <h2
+            className="w-full overflow-hidden truncate whitespace-nowrap text-center text-xl font-semibold text-dark_brown"
+            title={name}
+          >
+            {name}
+          </h2>
         </div>
       </div>
     </>
